4b0088efd05dd10cc142ffcc1321d87db60fbb75
Sundar K. Urs <sundarkurs@gmail.com>
2017-02-03 17:48:28 +0530

    Initial Startup
    Clients and InMemory user

---
+++ Paradise/IdentityServer.Web/Configuration/Clients.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using IdentityServer3.Core.Models;
+
+namespace IdentityServer.Web.Configuration
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+            {
+            new Client
+            {
+                Enabled = true,
+                ClientName = "MVC Client",
+                ClientId = "mvc",
+                Flow = Flows.Implicit,
+
+                RedirectUris = new List<string>
+                {
+                    "https://localhost:44319/"
+                },
+
+                AllowAccessToAllScopes = true
+            }
+        };
+        }
+    }
+}
---
+++ Paradise/IdentityServer.Web/Configuration/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Models;
+using Owin;
+
+namespace IdentityServer.Web.Configuration
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/identity", idsrvApp =>
+            {
+                idsrvApp.UseIdentityServer(new IdentityServerOptions
+                {
+                    SiteName = "Embedded IdentityServer",
+                    SigningCertificate = LoadCertificate(),
+
+                    Factory = new IdentityServerServiceFactory()
+                                .UseInMemoryUsers(Users.Get())
+                                .UseInMemoryClients(Clients.Get())
+                                .UseInMemoryScopes(StandardScopes.All)
+                });
+            });
+        }
+
+        X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                string.Format(@"{0}\bin\identityServer\idsrv3test.pfx", AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
+        }
+    }
+}
