10a53e551b892433274e2f96c4b313b68440c804
Sundar K. Urs <sundarkurs@gmail.com>
2017-02-07 18:02:46 +0530

    IdentityServer3 package added to client

--- Paradise/SecuredClient.Web/IdentityConfigurations/Startup.cs
+++ Paradise/SecuredClient.Web/IdentityConfigurations/Startup.cs
@@ -1,11 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using IdentityServer3.Core;
+using Microsoft.Owin;
 
+[assembly: OwinStartup(typeof(SecuredClient.Web.IdentityConfigurations.Startup))]
 namespace SecuredClient.Web.IdentityConfigurations
 {
     public class Startup
     {
+        public void Configuration(IAppBuilder app)
+        {
+
+            //AntiForgeryConfig.UniqueClaimTypeIdentifier = Constants.ClaimTypes.Subject;
+            //JwtSecurityTokenHandler.InboundClaimTypeMap = new Dictionary<string, string>();
+
+            // Middleware settings
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Cookies"
+            });
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                Authority = "https://localhost:44329/identity",
+                ClientId = "mvc",
+                Scope = "openid profile roles",
+                RedirectUri = "https://localhost:44329/",
+                ResponseType = "id_token",
+
+                SignInAsAuthenticationType = "Cookies",
+
+                UseTokenLifetime = false,
+
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    SecurityTokenValidated = n =>
+                    {
+                        var id = n.AuthenticationTicket.Identity;
+
+                        // we want to keep first name, last name, subject and roles
+                        var givenName = id.FindFirst(Constants.ClaimTypes.GivenName);
+                        var familyName = id.FindFirst(Constants.ClaimTypes.FamilyName);
+                        var sub = id.FindFirst(Constants.ClaimTypes.Subject);
+                        var roles = id.FindAll(Constants.ClaimTypes.Role);
+
+                        // create new identity and set name and role claim type
+                        var nid = new ClaimsIdentity(
+                            id.AuthenticationType,
+                            Constants.ClaimTypes.GivenName,
+                            Constants.ClaimTypes.Role);
+
+                        nid.AddClaim(givenName);
+                        nid.AddClaim(familyName);
+                        nid.AddClaim(sub);
+                        nid.AddClaims(roles);
+
+                        // add some other app specific claim
+                        nid.AddClaim(new Claim("app_specific", "some data"));
+
+                        // Logout redirect
+                        nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
+
+                        n.AuthenticationTicket = new AuthenticationTicket(
+                            nid,
+                            n.AuthenticationTicket.Properties);
+
+                        return Task.FromResult(0);
+                    },
+
+                    // Logout redirect
+                    RedirectToIdentityProvider = n =>
+                    {
+                        if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
+                        {
+                            var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
+
+                            if (idTokenHint != null)
+                            {
+                                n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
+                            }
+                        }
+
+                        return Task.FromResult(0);
+                    }
+                }
+
+            });
+
+        }
+
+
     }
 }
