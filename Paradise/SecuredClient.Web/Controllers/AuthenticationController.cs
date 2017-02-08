c5774ce96d47df553c8465b44e653839d6f91910
Sundar K. Urs <sundarkurs@gmail.com>
2017-02-08 11:46:07 +0530

    Client update to include Signin and Signout

---
+++ Paradise/SecuredClient.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecuredClient.Web.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [Authorize]
+        public ActionResult Login()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Logout()
+        {
+            Request.GetOwinContext().Authentication.SignOut();
+            return Redirect("/");
+        }
+    }
+}
--- Paradise/SecuredClient.Web/Controllers/HomeController.cs
+++ Paradise/SecuredClient.Web/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Security.Claims;
 using System.Web.Mvc;
 
 namespace SecuredClient.Web.Controllers
@@ -16,9 +13,7 @@ namespace SecuredClient.Web.Controllers
         [Authorize]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            return View((User as ClaimsPrincipal).Claims);
         }
 
         public ActionResult Contact()
