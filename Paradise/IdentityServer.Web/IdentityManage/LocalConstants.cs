b6daea814428195784c198f13595f63d836483c7
Sundar K. Urs <sundarkurs@gmail.com>
2017-02-08 18:46:32 +0530

    Identity Admin

---
+++ Paradise/IdentityServer.Web/IdentityManage/LocalConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IdentityServer.Web.IdentityManage
+{
+    public class LocalConstants
+    {
+        public const string UserAdminConfig = "Paradise.Users";
+
+        public const string AdminConfig = "Paradise.Config";
+    }
+}
--- Paradise/IdentityServer.Web/Service/IdentityAdminManagerService.cs
+++ Paradise/IdentityServer.Web/Service/IdentityAdminManagerService.cs
@@ -1,29 +1,24 @@
-﻿using IdentityServer3.EntityFramework;
-using IdentityServer3.EntityFramework.Entities;
+﻿using IdentityAdmin.Configuration;
+using IdentityAdmin.Core;
+using IdentityServer.Web.IdentityManage;
+using IdentityServer3.Admin.EntityFramework;
+using IdentityServer3.Admin.EntityFramework.Entities;
 
 namespace IdentityServer.Web.Service
 {
-    //public class IdentityAdminManagerService : IdentityAdminCoreManager<IdentityClient, int, IdentityScope, int>
-    //{
-       
-    //}
+    public class IdentityAdminManagerService : IdentityAdminCoreManager<IdentityClient, int, IdentityScope, int>
+    {
+        public IdentityAdminManagerService()
+            : base(LocalConstants.AdminConfig)
+        {
+        }
+    }
 
-    //public class IdentityAdminManagerService : IdentityAdminCoreManager<IdentityClient, int, IdentityScope, int>
-    //{
-    //    /// <summary>
-    //    ///     Initializes a new instance of the <see cref="IdentityAdminManagerService" /> class.
-    //    /// </summary>
-    //    public IdentityAdminManagerService()
-    //        : base(Constants.IdentityAdminConfig)
-    //    {
-    //    }
-    //}
-
-    //public static class IdentityAdminManagerServiceExtensions
-    //{
-    //    public static void Configure(this IdentityAdminServiceFactory factory)
-    //    {
-    //        factory.IdentityAdminService = new Registration<IIdentityAdminService, IdentityAdminManagerService>();
-    //    }
-    //}
+    public static class IdentityAdminManagerServiceExtensions
+    {
+        public static void Configure(this IdentityAdminServiceFactory factory)
+        {
+            factory.IdentityAdminService = new Registration<IIdentityAdminService, IdentityAdminManagerService>();
+        }
+    }
 }
