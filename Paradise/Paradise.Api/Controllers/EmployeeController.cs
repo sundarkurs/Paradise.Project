76ff5943fb64984e098b70c7b8a5de683ceacba6
Sundar K. Urs <sundarkurs@gmail.com>
2017-06-02 12:46:58 +0530

    api changes

---
+++ Paradise/Paradise.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Web.Http;
+using Paradise.Api.Legacy.Service;
+using Paradise.Api.Models;
+
+namespace Paradise.Api.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public Employee Get(int id)
+        {
+            ContactService service = new ContactService();
+            var contact = service.GetContact(id);
+
+            return new Employee()
+            {
+                Id = contact.ContactId,
+                FirstName = contact.ContactFirstName,
+                LastName = contact.ContactLastName,
+                Address = contact.ContactAddress,
+                Mobile = contact.ContactMobile,
+                DateOfBirth = contact.ContactDateOfBirth
+            };
+
+        }
+    }
+}
