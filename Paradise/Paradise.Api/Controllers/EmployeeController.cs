d0cd3d4610a9ccb5722f978feb247c6f631cb217
Sundar K. Urs <sundarkurs@gmail.com>
2017-06-02 13:18:45 +0530

    mapper working

--- Paradise/Paradise.Api/Controllers/EmployeeController.cs
+++ Paradise/Paradise.Api/Controllers/EmployeeController.cs
@@ -1,5 +1,8 @@
 ﻿
+using System;
 using System.Web.Http;
+using AutoMapper;
+using Paradise.Api.Legacy.Entities;
 using Paradise.Api.Legacy.Service;
 using Paradise.Api.Models;
 
@@ -12,16 +15,40 @@ namespace Paradise.Api.Controllers
             ContactService service = new ContactService();
             var contact = service.GetContact(id);
 
-            return new Employee()
+
+            //var config = new MapperConfiguration(cfg =>
+            //{
+            //    cfg.CreateMap<ContactEntity, Employee>();
+            //});
+
+            //IMapper mapper = config.CreateMapper();
+            //var source = new ContactEntity();
+
+            Mapper.Initialize(cfg =>
             {
-                Id = contact.ContactId,
-                FirstName = contact.ContactFirstName,
-                LastName = contact.ContactLastName,
-                Address = contact.ContactAddress,
-                Mobile = contact.ContactMobile,
-                DateOfBirth = contact.ContactDateOfBirth
-            };
+                cfg.CreateMap<ContactEntity, Employee>()
+                .ForMember(vm => vm.Id, map => map.MapFrom(s => s.ContactId))
+               .ForMember(vm => vm.FirstName, map => map.MapFrom(s => s.ContactFirstName))
+               .ForMember(vm => vm.LastName, map => map.MapFrom(s => s.ContactLastName))
+               .ForMember(vm => vm.Address, map => map.MapFrom(s => s.ContactAddress))
+               .ForMember(vm => vm.Mobile, map => map.MapFrom(s => s.ContactMobile))
+               .ForMember(vm => vm.DateOfBirth, map => map.MapFrom(s => s.ContactDateOfBirth));
+            });
+
+            return AutoMapper.Mapper.Map<Employee>(contact);
+
+            //return new Employee()
+            //{
+            //    Id = contact.ContactId,
+            //    FirstName = contact.ContactFirstName,
+            //    LastName = contact.ContactLastName,
+            //    Address = contact.ContactAddress,
+            //    Mobile = contact.ContactMobile,
+            //    DateOfBirth = contact.ContactDateOfBirth
+            //};
 
         }
     }
+    
+
 }
